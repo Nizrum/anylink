6edc075e322b0116a82e9831211c842e44910481
Nizrum <62435919+Nizrum@users.noreply.github.com>
2020-06-16 17:54:57 +0400

    Добавлены файлы

---
+++ script.js
@@ -0,0 +1,7 @@
+const input = document.querySelector('.input');
+const button = document.querySelector('.button');
+const link = document.querySelector('.link');
+
+button.addEventListener('click', function () {
+    link.href = input.value;
+});
